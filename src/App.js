793ad580403148c5a22acb6582866d071292f304
NaftiSirine <naftisirin30@gmail.com>
2023-03-06 17:13:09 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
-import TemplateFront from './components/front/TemplateFront';
-import TemplateBack from './components/back/TemplateBack';
+import { Route, Routes } from 'react-router-dom';
+import React, { Suspense } from 'react';
+const Login = React.lazy(() => import('./pages/user/Login'))
+const Register = React.lazy(()=> import ('./pages/user/Register'))
+const TemplateFront = React.lazy(()=> import ('./components/front/TemplateFront'))
+const TemplateBack = React.lazy(()=> import ('./components/back/TemplateBack'))
 
 function App() {
   return (
-<TemplateBack/>
+    <Suspense fallback={<div>Loading...</div>}>
+    <Routes>
+    <Route path='*' element={<TemplateFront/>}></Route>
+
+      <Route path='/login'  element={<Login />}></Route>
+  
+      <Route path='/register' element={<Register />}></Route>
+         
+      <Route path='/admin' element={<TemplateBack />}></Route>
+
+
+  </Routes>
+  </Suspense>
   );
 }
 
