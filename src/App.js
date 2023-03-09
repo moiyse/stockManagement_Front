e7d54ff960cbd778dbc8309d0aec1112f073c9d9
ala-hamadi <ala.hamadi.01@gmail.com>
2023-03-09 03:06:18 +0100

    Integrate reset password for user

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import React, { Suspense } from "react";
+import Layout from "./components/layout";
+import Settings from "./pages/user/Settings";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:5000";
 
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import React, { Suspense } from 'react';
-import Layout from './components/layout';
-const Register = React.lazy(()=> import ('./pages/user/Register'))
-const TemplateFront = React.lazy(()=> import ('./components/front/TemplateFront'))
-const TemplateBack = React.lazy(()=> import ('./components/back/TemplateBack'))
+const Register = React.lazy(() => import("./pages/user/Register"));
+const TemplateFront = React.lazy(() =>
+  import("./components/front/TemplateFront")
+);
+const TemplateBack = React.lazy(() => import("./components/back/TemplateBack"));
 
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Layout>
-    <Routes>
-    <Route path='*' element={<TemplateFront/>}/>
-  
-      <Route path='/register' element={<Register />}/>
-         
-      <Route path='/admin' element={<TemplateBack />}/>
+        <Routes>
+          <Route path='*' element={<TemplateFront />} />
 
+          <Route path='/register' element={<Register />} />
 
-  </Routes>
-  </Layout>
-  </Suspense>
+          <Route path='/admin' element={<TemplateBack />} />
+          <Route path='/settings' element={<Settings />} />
+        </Routes>
+      </Layout>
+    </Suspense>
   );
 }
 
